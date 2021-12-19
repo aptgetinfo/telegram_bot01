28e235a2d3f6e0045716108c4e0302ac5260f197
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-19 20:47:53 +0530

    inital message success

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const fetch =require('cross-fetch');
-const app = express();
-const API_KEY=process.env.API_KEY;
-const BASE_URI="https://api.telegram.org/"+API_KEY+"/getChatAdministrators?chat_id=@";
-app.use(cors());
-app.use(express.json());
-app.get('/', async (_, res) => {
-    try {
-        res.send('<h1>EventListnerOne:Online</h1>');
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-app.get('/getAdminOfGroup/:groupName', async (req,res)=>{
-    const array=[];
-   const _id = req.params.groupName;
-    if (!_id) {
-        res.status(400);
-    }
-    try {
-    const Ress = await fetch(BASE_URI + _id);
-    if (!Ress.ok) {
-        res.status(500);
-    }
-    const json = await Ress.json();
-    for(var i =0; i<json.result.length;i++){
-        array.push(json.result[i].user.username);
-    }
-        res.status(201).send(array);
-    } catch (e) {
-        res.status(404).send(e);
-    }
-});
-const PORT = process.env.PORT;
-const server = app.listen(PORT, () =>
-    console.log(`Sever running on port ${PORT}`)
-);
-process.on("unhandledRejection", (err, promise) => {
-    console.log(`Logged Error: ${err.message}`);
-    server.close(() => process.exit(1));
-});
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+const input = require("input"); // npm i input
+
+const apiId = process.env.apiId;
+const apiHash = process.env.apiHash;
+const stringSession = new StringSession(""); // fill this later with the value from session.save()
+
+(async () => {
+  console.log("Loading interactive example...");
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+  await client.start({
+    phoneNumber: async () => await input.text("Please enter your number: "),
+    password: async () => await input.text("Please enter your password: "),
+    phoneCode: async () =>
+      await input.text("Please enter the code you received: "),
+    onError: (err) => console.log(err),
+  });
+  console.log("You should now be connected.");
+  console.log(client.session.save()); // Save this string to avoid logging in again
+  await client.sendMessage("kernel_panic0", { message: "Hello!" });
+})();
