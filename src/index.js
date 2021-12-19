50eedac040d330f3e3a58c86f9b111f914d35580
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-20 02:08:12 +0530

    code complete env added

--- src/index.js
+++ src/index.js
@@ -1,87 +1,83 @@
 const express = require('express');
 const cors = require('cors');
 const input = require("input");
-const fetch =require('cross-fetch');
+const fetch = require('cross-fetch');
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
+const API_KEY = process.env.API_KEY;
+const API_LINK = "https://api.telegram.org/" + API_KEY + "/getChatAdministrators?chat_id=@";
+const API_ID = process.env.API_ID;
+const API_HASH = process.env.API_HASH;
+const STRING_SESSION = process.env.STRING_SESSION;
+const PORT = process.env.PORT;
+
+
 const app = express();
-const API_KEY=process.env.API_KEY;
-const BASE_URI="https://api.telegram.org/"+API_KEY+"/getChatAdministrators?chat_id=@";
-const apiId = process.env.API_ID;
-const apiHash = process.env.API_HASH;
-
-
 app.use(cors());
 app.use(express.json());
 
-const stringSession = new StringSession(process.env.STRING_SESSION); 
 
+const stringSession = new StringSession(STRING_SESSION);
+const client = new TelegramClient(stringSession, API_ID, API_HASH, {
+    connectionRetries: 5,
+});
 
 
 app.get('/', async (_, res) => {
     try {
-        res.send('<h1>EventListnerOne:Online</h1>');
+        res.send('<h1>Telegram Mesenger : Online</h1>');
     } catch (e) {
         res.status(500).send();
     }
 });
-app.get('/getAdminOfGroup/:groupName', async (req,res)=>{
-    const array=[];
-   const _id = req.params.groupName;
-    if (!_id) {
-        res.status(400);
-    }
-    try {
-    const Ress = await fetch(BASE_URI + _id);
-    if (!Ress.ok) {
-        res.status(500);
-    }
-    const json = await Ress.json();
-    for(var i =0; i<json.result.length;i++){
-        array.push(json.result[i].user.username);
-    }
-        res.status(201).send(array);
-    } catch (e) {
-        res.status(404).send(e);
-    }
-});
 
-app.get('/msgAdminOfGroup/:groupName', async (req,res)=>{
-    const array=[];
-   const _id = req.params.groupName;
+
+app.get('/getAdminOfGroup/:groupName', async (req, res) => {
+    const array = [];
+    const _id = req.params.groupName;
     if (!_id) {
-        res.status(400);
+        res.status(400).json({ failure: "Invalid Parameters" });
     }
     try {
-    const Ress = await fetch(BASE_URI + _id);
-    if (!Ress.ok) {
-        res.status(500);
-    }
-    const json = await Ress.json();
-    (async () => {
-        const client = new TelegramClient(stringSession, apiId, apiHash, {
-          connectionRetries: 5,
-        });
-        await client.start({
-          phoneNumber: async () => await input.text("Please enter your number: "),
-          password: async () => await input.text("Please enter your password: "),
-          phoneCode: async () =>
-            await input.text("Please enter the code you received: "),
-          onError: (err) => console.log(err),
-        });
-        console.log(client.session.save());
-        for(var i =0; i<json.result.length;i++){
-            await client.sendMessage(`${json.result[i].user.username}`, { message: "Hello!" });
+        const Ress = await fetch(API_LINK + _id);
+        if (!Ress.ok) {
+            res.status(500).json({ failure: "Group Not Found" });
         }
-      })();
-    
+        const json = await Ress.json();
+        for (var i = 0; i < json.result.length; i++) {
+            array.push(json.result[i].user.username);
+        }
+        res.status(200).send(array);
+    } catch (e) {
+        res.status(404).send(e);
+    }
+});
+
+app.post('/msgAdminOfGroup/:groupName', async (req, res) => {
+    const _id = req.params.groupName;
+    const { message } = req.body;
+    if (!_id || !message) {
+        res.status(400).json({ failure: "Invalid Parameters" });
+    }
+    try {
+        const Ress = await fetch(API_LINK + _id);
+        if (!Ress.ok) {
+            res.status(500).json({ failure: "Group Not Found" });
+        }
+        const json = await Ress.json();
+        if (!client) {
+            res.status(500).json({ failure: "Client not Found !" });
+        }
+        for (var i = 0; i < json.result.length; i++) {
+            await client.sendMessage(`${json.result[i].user.username}`, { message: `${message}` });
+        };
+        res.status(200).json({ sucess: true });
     } catch (e) {
         res.status(404).send(e);
     }
 });
 
 
-const PORT = process.env.PORT;
 const server = app.listen(PORT, () =>
     console.log(`Sever running on port ${PORT}`)
 );
